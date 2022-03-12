cbae941377cd2e1573ecf3d4ca129a21ab6c2678
hugosenari <hugosenari@gmail.com>
2022-03-12 15:32:09 -0300

    chore: re index syscall

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Raw system calls for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-syscall-master.flake = false;
+  inputs.src-syscall-master.owner = "def-";
+  inputs.src-syscall-master.ref   = "refs/heads/master";
+  inputs.src-syscall-master.repo  = "nim-syscall";
+  inputs.src-syscall-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-syscall-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-syscall-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
